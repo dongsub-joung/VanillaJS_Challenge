30f9f9441c009ce193780c8840b840aa661be664
dongsub-joung <59364300+dongsub-joung@users.noreply.github.com>
2020-02-06 06:51:39 +0900

    m

--- Code Challenge/JS Casino/index.js
+++ Code Challenge/JS Casino/index.js
@@ -1,44 +1,61 @@
-const numberId= document.getElementById("numberId"),
-playId= document.getElementById("playId");
 
-
-const numberKey="NUMBER"
-
-//숫자를 랜덤으로 생성
+//컴퓨터가 랜덤으로 숫자생성
+const machineInput= document.getElementById("machineInput");
 function createNumber(){
-    Math.floor(Math.random()*130);
+    let choseM= Math.floor(Math.random()*rangeScroll.value+1);
+    machineInput.innerHTML= `${choseM}`;
+    const M=  `${choseM}`;
+    return `${M}`;
 }
+
+//유저에게 숫자 입력받기
+const userInput= document.getElementById("userInput");
+function inputUser(){
+    let numberIdVal= document.getElementById("numberId").value;
+    userInput.innerHTML= `${numberIdVal}`;
+    const U= `${numberIdVal}`;
+    return U;
+}
+
+//두 수의 비교
+const result= document.getElementById("result");
+function compare(){
+    if( userInput === machineInput){
+        result.innerText=`U Win`;
+    } else{
+        result.innerText=`U rose`;
+    }
+}
+
+//submit x 
+const form= document.querySelector("form");
+function handleSubmit(e){
+    e.preventDefault();
+    inputUser();
+}
+
+const sliderValueView= document.getElementById('sliderValueView');
+const rangeScroll= document.getElementById(`rangeScroll`);
+function scroll(){ //확정
+    rangeScroll.setAttribute("oninput", "sliderValueView.innerHTML=rangeScroll.value");
+}
+
+const playId= document.getElementById("playId");
+function init(){
+    form.addEventListener("submit", handleSubmit); //확정
+    playId.addEventListener("click", createNumber);
+    scroll(); //확정
+    createNumber(); //비교
+    inputUser()
+}
+
+init();
+
+
+/*
+const numberKey="NUMBER"
 
 function saveNumber(num){
     localStorage.setItem(numberKey, `${num}`);
 }
-
-//숫자 입력받기
-const numberClass= document.querySelector(".numberClass");
-function inputNumber(){
-    const num= document.getElementById("numberId").value;
-    numberClass.addEventListener("submit", saveNumber(num));
-}
-
-
-let rangeScroll= document.getElementById("rangeScroll").value
-
-let selectedNum= document.querySelector(".selectedNum");
-/*
-function scroll(){
-    rangeScroll.addEventListener("change", changeRange);
-}
 */
-
-function changeRange(rangeScroll){
-    selectedNum =`Generate a number beween 0 and ${rangeScroll}`;
-    return selectedNum.textContent;
-}
-
-
-function init(){
-    scroll();
-    changeRange(rangeScroll);
-}
-
-init();
