82a143a4902469a1816f52d4b549ba9e317a9b62
dongsub-joung <59364300+dongsub-joung@users.noreply.github.com>
2020-02-07 17:16:03 +0900

    0207

--- Code Challenge/JS Calculator/index.js
+++ Code Challenge/JS Calculator/index.js
@@ -1,5 +1,43 @@
-const body= document.querySelector("body"),
- button= body.querySelectorAll("button");
-console.log(button);
+const calculator= document.getElementById("calculator");
+/*
+function add(num){
+    calculator.value= calculator.value + num;
+}
+*/
+let tOf= false; // 2번 연속 연산자면 1, 아니면 0
+function add(num){
+    if(tOf === false){
+        if( overlap(num) === true){
 
-//num, muti, result ,reset
+        } else {
+            calculator.value += num;
+            calculator.value= eval(calculator.value);
+        }
+    }else{
+        calculator.value += num;
+        calculator.value= eval(calculator.value);
+    }
+}
+const multi= document.querySelector(".multi");
+console.log(multi);
+
+function overlap(num){
+    if(num == String){
+        tOf= true;
+    } else{ 
+        tOf= false;
+    }
+} 
+/*
+function turnOf(){
+    return tOf=false;
+}
+*/
+const equals= document.getElementById("equals");
+function result(){
+    calculator.value= eval(calculator.value);
+}
+
+function reset(){
+    calculator.value="";
+}
