5ef460bc2bbf85df6338eacc485e7dec5b6c8ccb
dongsub-joung <59364300+dongsub-joung@users.noreply.github.com>
2020-02-08 21:38:37 +0900

    0208

---
+++ Code Challenge/Momontom/clock.js
@@ -0,0 +1,17 @@
+let div= document.querySelector(`.div`);
+let clock= document.querySelector(".clock");
+
+let clockSec= document.querySelector("#clockSec");
+function getDate(){
+    let toDay= new Date();
+    let hours= toDay.getHours(),
+     min= toDay.getMinutes(),
+     sec= toDay.getSeconds();
+    clock.innerHTML= `
+        ${hours < 10 ? `0${hours}` : hours}
+        : ${min < 10 ? `0${min}` : min}
+    `
+    clockSec.innerHTML = `:${sec < 10 ? `0${sec}` : sec}`
+}
+
+setInterval(getDate, 1000);
---
+++ Code Challenge/Momontom/username.js
@@ -0,0 +1,14 @@
+const user= document.querySelector(".user");
+
+if (user !== null){
+    // show
+} else {
+    // hide & paint
+}
+
+//paint
+const userName_CN=`userName`;
+const userName= document.querySelector(".userName");
+function paint(){
+    userName.classList(userName_CN);
+}
