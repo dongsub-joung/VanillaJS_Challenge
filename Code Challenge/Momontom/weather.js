8a82b64714e45a5a6345cfe8e86c2961e54fb08d
dongsub-joung <59364300+dongsub-joung@users.noreply.github.com>
2020-02-11 18:54:28 +0900

    21

--- Code Challenge/Momontom/weather.js
+++ Code Challenge/Momontom/weather.js
@@ -1,54 +1,54 @@
-const weather= document.querySelector(".js-weather");
+const weather= document.querySelector(".weather");
+
 const API_KEY= "0621aa6fcfa11ddbca0e493107ec7d6d";
-const COORDS=`coords`;
+const Weather_Val="Weather";
 
-function getWeather(lat, lng){  
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
-    .then(function(response) {
-        return response.json();
-    }).then(function(json){
-        const temperatuer= json.main.temp;  //console.log에서 정보를 찾아서 id를 찾아옴
-        const place= json.name;
-        weather.innerText=`${temperatuer} @ ${place}`;  
-    });
-}
-
-function saveCoords(coordsObj){
-    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
-}
-
-//getCurrentPosition requierment function start
-function handleGeoSucces(position){
+function handleGeo(position){   
     const latitude= position.coords.latitude;
     const longitude= position.coords.longitude;
-    const coordsObj={
+    const crdObj={
         latitude,
         longitude
     };
-    saveCoords(coordsObj);
+    save(crdObj);
     getWeather(latitude, longitude);
 }
 
-function handleGeoError(position){
-    console.log(`cant access geo location`);
-}                                               //getCurrentPosition end
-
-function askForCoords(){
-    navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError)
+function getWeather(latitude, longitude){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+    .then(response=> {
+        return response.json();
+    }).then(json => {
+        const temperatuer= json.main.temp;
+        const place= json.name;
+        weather.innerText=`${temperatuer} in ${place}`;
+    });
 }
 
-function loadCoords(){
-    const loadedCoords= localStorage.getItem(COORDS); //=coords
-    if(loadedCoords === null){
-        askForCoords();
-    }else{
-        const parseCoords= JSON.parse(loadedCoords);
-        getWeather(parseCoords.latitude, parseCoords.longitude);
+function save(crdObj){
+    localStorage.setItem(Weather_Val, JSON.stringify(crdObj));
+}
+
+function load(url){
+    const key= localStorage.getItem(Weather_Val);
+    if(key !== null){
+        const loadWeather= JSON.parse(key);
+        getWeather(loadWeather.latitude, loadWeather.longitude);
+    } else{
+        askGeo();
     }
 }
 
+function askGeo(){
+    navigator.geolocation.getCurrentPosition(handleGeo, handleError);
+}
+
+function handleError(){
+    console.log(`cant access geo location`);
+}
+
 function init(){
-    loadCoords();   //좌표를 요청하는 함수를 만들어보자
+    load();
 }
 
 init();
