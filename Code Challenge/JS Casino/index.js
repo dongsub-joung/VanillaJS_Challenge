7ec047a3dd6c6f14f2874c45b773ea9590ee9513
dongsub-joung <59364300+dongsub-joung@users.noreply.github.com>
2020-02-13 12:47:41 +0900

    a

--- Code Challenge/JS Casino/index.js
+++ Code Challenge/JS Casino/index.js
@@ -1,52 +1,47 @@
 
-//컴퓨터가 랜덤으로 숫자생성
-const machineInput= document.getElementById("machineInput");
+const machineInput= document.getElementById("machineInput"),
+ playId= document.getElementById("playId"),
+ sliderValueView= document.getElementById('sliderValueView'),
+ rangeScroll= document.getElementById(`rangeScroll`),
+result= document.getElementById("result"), 
+ form= document.querySelector("form"),
+ userInput= document.getElementById("userInput"),
+ numberId= document.getElementById("numberId");
+
+const show= document.querySelector(".show"),
+ showTwo= document.querySelector(".showTwo"); 
+
+
+function handleSubmit(e){
+    e.preventDefault();
+    inputUser();
+    show.classList.add("on");
+}
+
+function inputUser(){   //입력받은 값 출력
+    userInput.innerHTML= numberId.value;
+}
+
 function createNumber(){
-    let choseM= Math.floor(Math.random()*rangeScroll.value+1);
-    machineInput.innerHTML= `${choseM}`;
-    const M=  `${choseM}`;
-    return `${M}`;
-}
+    let choseM= Math.floor(Math.random()*rangeScroll.value+1);  //숫자 생성
+    machineInput.innerHTML= choseM;    //출력
+    showTwo.classList.add("on");
 
-//유저에게 숫자 입력받기
-const userInput= document.getElementById("userInput");
-function inputUser(){
-    let numberIdVal= document.getElementById("numberId").value;
-    userInput.innerHTML= `${numberIdVal}`;
-    const U= `${numberIdVal}`;
-    return U;
-}
-
-//두 수의 비교
-const result= document.getElementById("result");
-function compare(){
-    if( userInput === machineInput){
+    if( numberId.value == choseM){
         result.innerText=`U Win`;
     } else{
         result.innerText=`U rose`;
     }
 }
 
-//submit x 
-const form= document.querySelector("form");
-function handleSubmit(e){
-    e.preventDefault();
-    inputUser();
-}
-
-const sliderValueView= document.getElementById('sliderValueView');
-const rangeScroll= document.getElementById(`rangeScroll`);
-function scroll(){ //확정
+function scroll(){ 
     rangeScroll.setAttribute("oninput", "sliderValueView.innerHTML=rangeScroll.value");
 }
 
-const playId= document.getElementById("playId");
 function init(){
-    form.addEventListener("submit", handleSubmit); //확정
+    form.addEventListener("submit", handleSubmit); 
     playId.addEventListener("click", createNumber);
-    scroll(); //확정
-    createNumber(); //비교
-    inputUser()
+    scroll(); 
 }
 
 init();
